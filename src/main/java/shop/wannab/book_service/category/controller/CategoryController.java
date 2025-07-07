f6817344612f73fe93381d481d6cc58fa20ad0b9
dkyun97 <67477254+dkyun97@users.noreply.github.com>
2025-07-07 10:27:52 +0900

    feat: 카테고리 도서 조회 기능

--- src/main/java/shop/wannab/book_service/category/controller/CategoryController.java
+++ src/main/java/shop/wannab/book_service/category/controller/CategoryController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import shop.wannab.book_service.category.dto.CategoryCreateRequest;
 import shop.wannab.book_service.category.dto.CategoryHierarchyDto;
 import shop.wannab.book_service.category.dto.ParentCategoryDto;
 import shop.wannab.book_service.category.service.CategoryService;
