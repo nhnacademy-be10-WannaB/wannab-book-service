42bd87df4b5040e2a4c3ec57713ecccfebd8220b
dkyun97 <67477254+dkyun97@users.noreply.github.com>
2025-07-07 10:27:16 +0900

    feat: 카테고리 도서 조회 기능

---
+++ src/main/java/shop/wannab/book_service/category/dto/ParentCategoryDto.java
@@ -0,0 +1,13 @@
+package shop.wannab.book_service.category.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ParentCategoryDto {
+    private Long id;
+    private String name;
+}
--- src/main/java/shop/wannab/book_service/category/service/CategoryService.java
+++ src/main/java/shop/wannab/book_service/category/service/CategoryService.java
@@ -1,10 +1,14 @@
 package shop.wannab.book_service.category.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import shop.wannab.book_service.book.entity.BookCategory;
+import shop.wannab.book_service.book.repository.BookCategoryRepository;
+import shop.wannab.book_service.category.dto.ParentCategoryDto;
 import shop.wannab.book_service.category.entity.Category;
 import shop.wannab.book_service.category.dto.CategoryHierarchyDto;
 import shop.wannab.book_service.category.repository.CategoryRepository;
@@ -14,11 +18,10 @@ import shop.wannab.book_service.category.repository.CategoryRepository;
 @RequiredArgsConstructor
 public class CategoryService {
     private final CategoryRepository categoryRepository;
+    private final BookCategoryRepository bookCategoryRepository;
 
-    //재귀적으로 카테고리를 반환해주는 코드
     @Transactional(readOnly = true)
     public List<CategoryHierarchyDto> getCategoryHierarchy() {
-        //최상위 카테고리 호출
         List<Category> rootCategories = categoryRepository.findByParentIsNull();
 
         List<CategoryHierarchyDto> categoryHierarchyDtoList = new ArrayList<>();
@@ -28,5 +31,29 @@ public class CategoryService {
             categoryHierarchyDtoList.add(dto);
         }
         return categoryHierarchyDtoList;
+    }
+
+    @Transactional
+    public Long getCategoryId(Long bookId){
+        List<BookCategory> bookCategories = bookCategoryRepository.findCategoriesByBookIdWithFetchJoin(bookId);
+
+        if (bookCategories.isEmpty()) {
+            throw new EntityNotFoundException("해당 책에 대한 카테고리 정보를 찾을 수 없습니다. bookId: " + bookId);
+        }
+
+        return bookCategories.get(0).getCategory().getId();
+    }
+
+    @Transactional(readOnly = true)
+    public List<ParentCategoryDto> getParentCategory() {
+        List<Category> rootCategories = categoryRepository.findByParentIsNull();
+        List<ParentCategoryDto> parentCategoryDtoList = new ArrayList<>();
+        for (Category rootCategory : rootCategories) {
+            ParentCategoryDto parentCategoryDto = new ParentCategoryDto(
+                    rootCategory.getId(),
+                    rootCategory.getName());
+            parentCategoryDtoList.add(parentCategoryDto);
+        }
+        return parentCategoryDtoList;
     }
 }
