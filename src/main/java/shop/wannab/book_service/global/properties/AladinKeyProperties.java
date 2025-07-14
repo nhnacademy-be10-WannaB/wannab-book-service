60135772bc0b3b27314b7669d81438984c6fcf17
gnsals0904 <gnsals0904@naver.com>
2025-07-14 13:49:32 +0900

    feat: 알라딘 Key Properties 클래스 구현

---
+++ src/main/java/shop/wannab/book_service/global/properties/AladinKeyProperties.java
@@ -0,0 +1,14 @@
+package shop.wannab.book_service.global.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter @Setter
+@Component
+@ConfigurationProperties(prefix = "aladin.api")
+public class AladinKeyProperties {
+    private String baseUrl;
+    private String ttbKey;
+}
