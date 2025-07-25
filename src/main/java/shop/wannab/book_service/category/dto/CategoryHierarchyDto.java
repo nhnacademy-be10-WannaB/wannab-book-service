5257d0092285728f27b20d1386e3ee17cae88a6a
gnsals0904 <gnsals0904@naver.com>
2025-07-25 14:30:42 +0900

    fix: 카테고리 dto 역직렬화 어노테이션 추가

--- src/main/java/shop/wannab/book_service/category/dto/CategoryHierarchyDto.java
+++ src/main/java/shop/wannab/book_service/category/dto/CategoryHierarchyDto.java
@@ -1,5 +1,6 @@
 package shop.wannab.book_service.category.dto;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
@@ -9,6 +10,11 @@ import shop.wannab.book_service.category.entity.Category;
 
 @Getter
 @Setter
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.CLASS,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "@class"
+)
 @NoArgsConstructor
 public class CategoryHierarchyDto {
     private Long id;
